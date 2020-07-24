8e1cf91901229cee8368e76c6667bcc2d4c3f43f
danielvornicu <vdany2003@yahoo.com>
2020-07-24 16:04:22 +0200

    update

--- SampleDjango/settings.py
+++ SampleDjango/settings.py
@@ -15,6 +15,14 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -25,7 +33,9 @@ SECRET_KEY = ')7rb93c9*85ogoepg2@2i3)z0m-+z1u4a*n*dxtj42r4-yz-)y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+     'floating-river-31233.herokuapp.com'
+]
 
 
 # Application definition
