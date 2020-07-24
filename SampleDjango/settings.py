6e12cb2292c73e643c5f350e7a24929eb40fd1d9
danielvornicu <vdany2003@yahoo.com>
2020-07-24 16:28:23 +0200

    update

--- SampleDjango/settings.py
+++ SampleDjango/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "clients/static")
 ]
